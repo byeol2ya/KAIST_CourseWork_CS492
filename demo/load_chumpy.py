12f2457559c24e8393983d2cf41d9ea9bb6ac826
Mingi Yeom <mingiyeom@kaist.ac.kr>
2021-02-19 18:40:42 +0900

    clean main
    extract obj file more easily

--- demo/load_chumpy.py
+++ demo/load_chumpy.py
@@ -48,40 +48,17 @@ def extract_obj():
 
     num_pose = 24 * 3
     num_betas = 300
-    # model = STAR(gender='female',num_betas=10)
+    model = STAR(gender='female',num_betas=10)
     # ## Assign random pose and shape parameters
-    # model.pose[:] = np.random.rand(model.pose.size) * .2
-    # model.betas[:] = np.random.rand(model.betas.size) * .03
-    #
     # for j in range(0,10):
     #     model.betas[:] = 0.0  #Each loop all PC components are set to 0.
     #     for i in np.linspace(-3,3,10): #Varying the jth component +/- 3 standard deviations
     #         model.betas[j] = i
-    ################################################################################################################
-    # betas = ch.array(np.zeros(num_betas)) #Betas
-    # pose = ch.array(np.zeros(num_pose))  # Pose
-    # pose = ch.array((np.random.rand(num_pose)) - 0.5) * 1
-    # print(pose)
-    # betas = ch.array(
-    #             np.array([ 2.25176191, -3.7883464, 0.46747496, 3.89178988,
-    #                       2.20098416, 0.26102114, -3.07428093, 0.55708514,
-    #                       -3.94442258, -2.88552087])) * .03
-    # betas_numpy = np.array([ 4.0, 0.0, 0.0, 0.0,
-    #                      0.0, 0.0, 0.0, 0.0,
-    #                      0.0, 0.0])
+    #         model.betas = ch.array(np.zeros(num_betas)) #Betas
+    model.shape = ch.array(np.zeros(num_betas))  # Pose
+    model.pose = ch.array(np.zeros(num_pose))  # Pose
 
-    # betas_numpy = np.zeros(num_betas)
-    # for i in range(0,num_betas):
-    #     betas_numpy[i] = i
-    # betas = ch.array(betas_numpy)
-
-    # for j in range(0,10):
-    #     model.betas[:] = 0.0  #Each loop all PC components are set to 0.
-    #     for i in np.linspace(-3,3,10): #Varying the jth component +/- 3 standard deviations
-    #         model.betas[j] = i
-
-    # save_as_obj(model, "./", name="output_2_0")
-    ################################################################################################################
+    save_as_obj(model, "./", name="output_2_0")
 
 
 def make_data(base_data=np.zeros(0)):
@@ -120,8 +97,9 @@ def make_data(base_data=np.zeros(0)):
     np.save('./saved_210217.npy', ret)
 
 def main():
-    ret_back = np.load('./saved_300betas_base.npy')
-    make_data(ret_back)
+    extract_obj()
+    # ret_back = np.load('./saved_300betas_base.npy')
+    # make_data(ret_back)
     # x_save_load = np.load('./saved.npy')
     # for i in range(0,10):
     #    print(x_save_load[i,:])
