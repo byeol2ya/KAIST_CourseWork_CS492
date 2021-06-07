11af5ea52da62c5cd109d8d8217bcf7e11b6289c
byeol2ya <byeol2@kaist.ac.kr>
2021-06-07 16:34:26 +0900

    delete FinalProject folder

--- FinalProject/STAR-Private/demo/main.py
+++
@@ -1,5 +0,0 @@
-from extract_shape_dif import *
-gender = "female"
-name = "f_weight"
-save_path = "E:/STAR/f_star/"
-extractor_weight_and_joint(gender=gender, save_path=save_path, name=name, isWeight=False)
